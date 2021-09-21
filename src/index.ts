46c8f6a05a9a0e0c6abd1af4b0ebd2c6b7cf2e0c
rob shaw <rob@statik.dev>
2021-09-21 00:08:02 -0400

    use mint address to get metadata and store in object

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import got from 'got';
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 
+import { Metadata } from './metaplex/classes';
 import { METADATA_PROGRAM_ID } from './metaplex/constants';
 import { decodeMetadata } from './metaplex/metadata';
 
 const METADATA_PROGRAM_PK = new PublicKey(METADATA_PROGRAM_ID);
+
+interface MintData {
+  metadata: Metadata;
+  attributes: any;
+  imageUri?: string;
+}
 
 (async function () {
   const mintWallet = 'AuTF3kgAyBzsfjGcNABTSzzXK4bVcZcyZJtpCrayxoVp'; // snek wallet mint
@@ -23,10 +31,21 @@ const METADATA_PROGRAM_PK = new PublicKey(METADATA_PROGRAM_ID);
   console.log('Found: ', response.length);
 
   const mintTokenIds = [];
+  const mints: MintData[] = [];
+
   for (const record of response) {
     const solMetadata = decodeMetadata(record.account.data);
+    const nftInfoResponse = await got(solMetadata.data.uri);
+    const attrs = JSON.parse(nftInfoResponse.body);
+
+    const mintData = {
+      metadata: solMetadata,
+      attributes: attrs,
+      imageUri: attrs?.image,
+    };
 
     mintTokenIds.push(solMetadata.mint);
+    mints.push(mintData);
   }
 
   console.log(mintTokenIds);
